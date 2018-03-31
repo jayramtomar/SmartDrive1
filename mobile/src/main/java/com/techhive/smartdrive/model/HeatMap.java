b75144dc7ba2647bbe6cdb5dcc643f865edec0a3
jayramtomar <jayramtomar@gmail.com>
2018-03-31 12:50:03 +0530

    Heat Map

---
+++ mobile/src/main/java/com/techhive/smartdrive/model/HeatMap.java
@@ -0,0 +1,46 @@
+package com.techhive.smartdrive.model;
+
+/**
+ * Created by uema on 3/31/2018.
+ */
+
+public class HeatMap {
+
+    double lattitude;
+    double longitude;
+    double category;
+    double level;
+
+
+    public double getLattitude() {
+        return lattitude;
+    }
+
+    public void setLattitude(double lattitude) {
+        this.lattitude = lattitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getCategory() {
+        return category;
+    }
+
+    public void setCategory(double category) {
+        this.category = category;
+    }
+
+    public double getLevel() {
+        return level;
+    }
+
+    public void setLevel(double level) {
+        this.level = level;
+    }
+}
